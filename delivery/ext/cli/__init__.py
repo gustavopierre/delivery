9a5dc1bf252282d8d72c21f2d5cb9d0464f56beb
Gustavo Pierre <gustavopierre@gmail.com>
2020-07-26 07:34:48 -0300

    DB with SQLAlchemy

---
+++ delivery/ext/cli/__init__.py
@@ -0,0 +1,35 @@
+import click
+from delivery.ext.db import db
+from delivery.ext.site import models  
+
+
+def init_app(app):
+    @app.cli.command()
+    def create_db():
+        """Este comando inicializa o db"""
+        db.create_all()
+
+    @app.cli.command()
+    @click.option("--email", "-e")
+    @click.option("--passwd", "-p")
+    @click.option("--admin", "-a", is_flag=True, default=False)
+    def add_user(email, passwd, admin):
+        """adiciona novo usuário"""
+        user = models.User(
+            email=email,
+            passwd=passwd,
+            admin=admin
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        click.echo(f"usuário {email} criado com sucesso!")
+
+    @app.cli.command()
+    def listar_pedidos():
+        # TODO; usar tabulate
+        click.echo("lista de pedidos")
+    
+    @app.cli.command()
+    def listar_usuarios():
+        click.echo("lista de usuários")
